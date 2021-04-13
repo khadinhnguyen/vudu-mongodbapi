e9855e9883563e4cc964f21b81222aa39242206b
Kha Nguyen <knguyen93@myseneca.ca>
2021-04-13 08:26:21 -0400

    initial project

---
+++ vudu-mongodb-api/src/main/java/com/nguyendkha/vudumongodbapi/VuduMongodbApiApplication.java
@@ -0,0 +1,13 @@
+package com.nguyendkha.vudumongodbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuduMongodbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuduMongodbApiApplication.class, args);
+	}
+
+}
---
+++ vudu-mongodb-api/src/test/java/com/nguyendkha/vudumongodbapi/VuduMongodbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyendkha.vudumongodbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuduMongodbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
